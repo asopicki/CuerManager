3e721fe52681de2b9545d56de65af2994f9b5b10
Alexander Sopicki <asopicki@gmail.com>
2018-01-07 09:21:33 +0100

    Fix store in production environment

--- ui/src/store/setupStore.prod.js
+++ ui/src/store/setupStore.prod.js
@@ -5,8 +5,8 @@ import rootReducer from '../reducers';
 
 const setupStore = preloadedState => createStore(
 	rootReducer,
-	Object.assign({}, preloadedState, {cuesheetSearch: { serverPort: 8000}}),
-	() => applyMiddleware(api)
+    Object.assign({}, preloadedState, {cuesheetSearch: { serverPort: 8000}}),
+    applyMiddleware(api)
 )
 
 export default setupStore
