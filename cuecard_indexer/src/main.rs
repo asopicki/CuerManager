b62f30bc9ce3c5b9be491c2e42828ffad65c37b4
Alexander Sopicki <asopicki@gmail.com>
2019-10-25 09:02:23 +0200

    structopt conversion

--- cuecard_indexer/src/main.rs
+++ cuecard_indexer/src/main.rs
@@ -1,28 +1,37 @@
-#[macro_use]
-extern crate clap;
+extern crate structopt;
 extern crate cuecard_indexer;
 extern crate env_logger;
+
+use structopt::StructOpt;
+use std::path::PathBuf;
+
 use std::env;
 
+#[derive(StructOpt, Debug)]
+#[structopt(name = "cuecard_indexer")]
+/// Creates an index of cue cards in a Sqlite database
+struct ProgramOptions {
+    #[structopt(short, long)]
+    /// Sets the path to the databbase to use
+    database: Option<String>,
+
+    #[structopt(parse(from_os_str))]
+    /// Sets the base directory for the cue card collection
+    input: PathBuf
+}
+
 fn main() {
-    use clap::App;
     env_logger::init();
+    let options = ProgramOptions::from_args();
 
-    let yml = load_yaml!("cli.yml");
-    let matches = App::from_yaml(yml).get_matches();
-
-    let database_option = matches.value_of("database");
-
-    let database_url = match database_option {
+    let database_url = match options.database {
         Some(opt) => opt.to_string(),
-        _ => env::var("DATABASE_URL").expect("DATABASE_URL must be set"),
+        _ => env::var("DATABASE_URL").expect("DATABASE_URL must be set")
     };
 
-    let basepath = matches.value_of("INPUT").unwrap();
-
     let config = cuecard_indexer::Config {
-        basepath: basepath.to_string(),
-        database_url,
+        basepath: options.input.to_str().expect("Base directory of the cue card collection exptected").to_string(),
+        database_url
     };
 
     cuecard_indexer::run(&config);
